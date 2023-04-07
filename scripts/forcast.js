ef9a38e4a95c1e5ba8e55a603aae1b4eed13788e
Pranshu1sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-04-07 12:36:26 +0530

    All Files Added

---
+++ scripts/forcast.js
@@ -0,0 +1,25 @@
+const key = "PUcm569Pw5yrP7qfq1N9PnASTrH1U8Z3"
+const getWeather = async(id) =>{
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const query = `${id}?apikey=${key}`
+    const response = await fetch(base+query)
+    const data = await response.json();
+    return data[0];
+}
+const getCity = async(city) =>{
+const base  = "http://dataservice.accuweather.com/locations/v1/cities/search";
+const query = `?apikey=${key}&q=${city}`;
+const response = await fetch(base + query);
+const data= await response.json();
+return data[0]
+};
+// getCity('manchester')
+// .then(data=>{return getWeather(data.Key);}).then(data=>{
+//     console.log(data);
+// })
+// .catch(err=>console.log(err))
+
+
+
+
+
