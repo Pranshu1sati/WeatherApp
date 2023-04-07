25ffa3019b0f7e29a9a527ad46833462635a70e4
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-04-07 12:44:36 +0530

    Update forcast.js

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,13 +1,13 @@
 const key = "PUcm569Pw5yrP7qfq1N9PnASTrH1U8Z3"
 const getWeather = async(id) =>{
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/"
     const query = `${id}?apikey=${key}`
     const response = await fetch(base+query)
     const data = await response.json();
     return data[0];
 }
 const getCity = async(city) =>{
-const base  = "http://dataservice.accuweather.com/locations/v1/cities/search";
+const base  = "https://dataservice.accuweather.com/locations/v1/cities/search";
 const query = `?apikey=${key}&q=${city}`;
 const response = await fetch(base + query);
 const data= await response.json();
